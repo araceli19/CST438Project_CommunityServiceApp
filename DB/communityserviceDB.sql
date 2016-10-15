0dcfd878b664517c299372ca38e496f749cb0512
Lesly Garcia <lgarciajimenez@csumb.edu>
2016-10-15 10:00:16 -0700

    added data to DB

--- DB/communityserviceDB.sql
+++ DB/communityserviceDB.sql
@@ -78,3 +78,36 @@ CREATE TABLE Pending_Volunteers(
 FOREIGN KEY (Available_ID) REFERENCES Available_Services (ID) ON UPDATE CASCADE ON DELETE RESTRICT
 
 );
+
+INSERT INTO  Website_Operator(ID, Name,Phone_Num, Email, Password)
+VALUES (1, 'Tom B. Erichsen','(831)222-2222','Stavanger@gmail.com','4006Norway');
+INSERT INTO  Website_Operator(Name,Phone_Num, Email, Password)
+VALUES (2, 'Liliana Jones','(831)123-2532', 'LilyJ@gmail.com','4005France');
+
+INSERT INTO Volunteer_Organization(ID, Contact_name, Org_Name, Phone_Num, Email, Password)
+VALUES(1, 'Andrea Humer', 'Otter Express', '(831)122-1234', 'ottterE@yahoo.com', '123London');
+INSERT INTO Volunteer_Organization(Contact_name, Org_Name, Phone_Num, Email, Password)
+VALUES(2, 'Miriam London', 'Monterey Bay Aquarium', '(831)130-1744', 'MLondon@hotmail.com', '123Louisiana');
+
+INSERT INTO Volunteer(ID, Name, DOB, School, School_ID, Hours, Phone_Num)
+VALUES(1, 'Cecilia Perez', '12-04-1998', 'Seaside High School', 27493, 0, '(563)593-5859’);
+INSERT INTO Volunteer(Name, DOB, School, School_ID, Hours, Phone_Num)
+VALUES(2, 'Ozi Benini', '03-14-2003', 'Palma Middle School', 78420, 8, '(612)502-5256’);
+INSERT INTO Volunteer(Name, DOB, School, School_ID, Hours, Phone_Num)
+VALUES(3, 'Jess Noel', '07-28-1991', 10, '(562)384-1596’);
+
+INSERT INTO Pending_Post(Provider_ID, Status)
+VALUES(2, 'Pending');
+INSERT INTO Pending_Post(Provider_ID, Status)
+VALUES(1, 'Approved');
+
+INSERT INTO Available_Services(Organization_ID, Hours_Available, Volunteers_Needed, Description, Name_Of_Service, Phone_Num)
+VALUES(1, 10, 5, 'Students needed to tutor first graders', 'Tutoring at Grace Elementary', '(347)390-0851');
+  INSERT INTO Available_Services(Organization_ID, Hours_Available, Volunteers_Needed, Description, Name_Of_Service, Phone_Num)
+VALUES(2, 5, 10, 'Community members needed to help with beach cleaning', 'Beach cleaning day', '(714)361-2381');
+
+INSERT INTO Current_Volunteers(Volunteer_ID, Hours)
+VALUES (2, 8);
+INSERT INTO Current_Volunteers(Volunteer_ID, Hours)
+VALUES (3, 10);
+

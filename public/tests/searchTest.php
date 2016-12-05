7c2598c3eaacb6fe3e22615d8c5e3efabc60d83c
araceli19 <agopar@csumb.edu>
2016-12-04 22:19:15 -0800

    moved all files from merged branch

--- public/tests/searchTest.php
+++ public/tests/searchTest.php
@@ -4,11 +4,9 @@ use Laracasts\Integrated\Extensions\Goutte as IntegrationTest;
 
 class searchTest extends IntegrationTest{
 
-
   protected $baseUrl = 'http://sample-env.8fm6rg3smv.us-west-2.elasticbeanstalk.com/';
     /** @test */
     function check_search_page_exists(){
-      //testing if the page exists and can be access in aws
       $this->visit('/')
       ->click('Services')
       ->seePageIs('/http://sample-env.8fm6rg3smv.us-west-2.elasticbeanstalk.com/public/html/Search.php')
@@ -17,8 +15,6 @@ class searchTest extends IntegrationTest{
     }
     /** @test */
     function check_search_page_search(){
-      //checks if the page to search shows up
-      //this uses an invalid search with no results
       $this->visit('/public/html/Search.php')
       ->andType('Integration testing', 'search')
       ->press('searchForm')
@@ -27,7 +23,6 @@ class searchTest extends IntegrationTest{
     }
     /** @test */
     function check_search_find_correctSearch(){
-      //checks for a valid search
       $this->visit('/public/html/Search.php')
       ->andType('Holiday Dinner', 'search')
       ->press('searchForm')
